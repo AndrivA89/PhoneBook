3534ad2c1989e07a08078d5b14b04d6abfdaea99
AndrivA <xxx3003@gmai.com>
2020-02-08 14:32:36 +0300

    commit

--- main.go
+++ main.go
@@ -1,27 +1,90 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"database/sql"
+	"html/template"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	var tempN int
-	// Получаем из параметров кол-во одновременно выполняемых задач
-	flag.IntVar(&tempN, "N", 1, "Количество одновременно выполняемых задач")
-	flag.Parse()
-	
-	r := mux.NewRouter()                                   // Создаем роутер для маршрутизации
-	
-    /*
-    r.HandleFunc("/AddTask", a.AddTask).Methods("POST")    // Объявление обработчика на добавление задачи
-	r.HandleFunc("/ListTasks", a.ListTasks).Methods("GET") // Объявление обработчика на вывод списка всех задач
-	r.HandleFunc("/", a.MainPage).Methods("GET")           // Объявление обработчика главной страницы
-    */
+type Contact struct {
+	Id          int
+	Name        string
+	PhoneNumber string
+}
 
-	fmt.Println("Сервер запущен на порту :80")             // Информация о запущенном сервере
-	log.Fatal(http.ListenAndServe(":80", r))               // Запуск сервера и проверка ошибок
+type Handler struct {
+	DB   *sql.DB
+	Tmpl *template.Template
+}
+
+func (h *Handler) MainPage(w http.ResponseWriter, r *http.Request) {
+	contacts := []*Contact{}
+	rows, err := h.DB.Query("SELECT id, name, phoneNumber FROM contacts")
+
+	//TODO: Обработка ошибки err
+	//...
+	//...
+
+	for rows.Next() {
+		currentContact := &Contact{}
+		err = rows.Scan(&currentContact.Id, &currentContact.Name, &currentContact.PhoneNumber)
+
+		//TODO: Обработка ошибки err
+		//...
+		//...
+
+		contacts = append(contacts, currentContact)
+	}
+	rows.Close()
+
+	err = h.Tmpl.ExecuteTemplate(w, "index.html", struct {
+		Contacts []*Contact
+	}{
+		Contacts: contacts,
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *Handler) Add(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) Edit(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) Update(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {}
+
+func main() {
+	dsn := "root@tcp(localhost)" // Логин-пароль к БД
+	dsn += "&charset=utf8"
+	dsn += "&interpolateParams=true"
+
+	db, err := sql.Open("mysql", dsn)
+	db.SetMaxOpenConns(10)
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	handlers := &Handler{
+		DB:   db,
+		Tmpl: template.Must(template.ParseGlob("../templates/*")),
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/contact/new", handlers.Add).Methods("POST")
+	r.HandleFunc("/contact/{id}", handlers.Edit).Methods("GET")
+	r.HandleFunc("/contact/{id}", handlers.Update).Methods("POST")
+	r.HandleFunc("/contact/{id}", handlers.Delete).Methods("DELETE")
+	r.HandleFunc("/", handlers.MainPage).Methods("GET")
+
+	log.Println("Сервер запущен на порту :80")
+	log.Fatal(http.ListenAndServe(":80", r))
 }
