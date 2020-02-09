be4d7a919ede64526808c2ab575f6c3c697e245c
Андрей Иващенко <andriva@MacBook-Pro-Andrej.local>
2020-02-09 14:48:28 +0300

    commit

--- main.go
+++ main.go
@@ -17,15 +17,21 @@ type Contact struct {
 	PhoneNumber string
 }
 
-// Структура для использования подключения к БД в обработчиках
-type Handler struct {
-	DB *sqlx.DB
-}
+// Переменная для подключения к БД
+var DB *sqlx.DB
+
+// Переменная для получения данных об ошибках
+var err error
 
 // Обработчик главной страницы - вывод всех контактов и телефонов
-func (h *Handler) MainPage(w http.ResponseWriter, r *http.Request) {
+func MainPage(w http.ResponseWriter, r *http.Request) {
 	contacts := []*Contact{}
-	rows, err := h.DB.Query("SELECT `contacts`.`id`, `contacts`.`name`, `phone_number`.`number` FROM `contacts`, `phone_number` WHERE `contacts`.`id` = `phone_number`.`contactsID`;")
+	// Создание запроса для выборки всех контактов
+	query := "SELECT `contacts`.`id`, `contacts`.`name`, `phone_number`.`number` "
+	query += "FROM `contacts`, `phone_number` "
+	query += "WHERE `contacts`.`id` = `phone_number`.`contactsID`;"
+
+	rows, err := DB.Query(query)
 	errorMsg(err, "Отправка запроса выбора данных из таблиц - функция MainPage")
 
 	for rows.Next() {
@@ -44,7 +50,7 @@ func (h *Handler) MainPage(w http.ResponseWriter, r *http.Request) {
 func Create(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	currentContact := &Contact{}
-	err := json.NewDecoder(r.Body).Decode(&currentContact)
+	err = json.NewDecoder(r.Body).Decode(&currentContact)
 	errorMsg(err, "Получение JSON - функция Create")
 }
 
@@ -61,19 +67,15 @@ func errorMsg(err error, comment string) {
 
 func main() {
 	ConnectionStringDB := "root:1234567890@tcp(localhost:3306)/phone_book"
-	conn, err := sqlx.Connect("mysql", ConnectionStringDB)
-	if err != nil {
-		panic(err)
-	}
-
-	handlers := &Handler{DB: conn}
+	DB, err = sqlx.Connect("mysql", ConnectionStringDB)
+	errorMsg(err, "Подключение к БД - функция main")
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/contact/new", Create).Methods("POST")
 	r.HandleFunc("/contact/{id}", Update).Methods("POST")
 	r.HandleFunc("/contact/{id}", Delete).Methods("DELETE")
-	r.HandleFunc("/", handlers.MainPage).Methods("GET")
+	r.HandleFunc("/", MainPage).Methods("GET")
 
 	log.Println("Сервер запущен на порту :88")
 	log.Fatal(http.ListenAndServe(":88", r))
